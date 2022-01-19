76c13a2c695905fe80ec19c7dc8289e7530b5e1c
Arushi Katyal <katy.arushi@gmail.com>
2022-01-18 19:23:37 -0500

    finish dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['JUNGLE_USERNAME'], password: ENV['JUNGLE_PASSWORD']
 
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
   end
 
 end
